64deaaaebe9a8275a435a7bc2fe25c82803c04e9
2076659780xmx <2076659780@qq.com>
2023-03-21 15:19:53 +0800

    chatgpt

---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://wangpan-1308863530.cos.ap-nanjing.myqcloud.com/uploads/2023/02/02/mmexport1674884718626.png',
+      name: '梦想',
+      description: '文案',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
